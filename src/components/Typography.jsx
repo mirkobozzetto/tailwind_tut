3834cdb9cdc8dc435e4f71bd5e4ea9574a10cddc
Mirko Bozzetto <mirkouch@gmail.com>
2024-02-19 16:35:38 +0100

    Ajouter des styles de typographie à la composante Typography

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -1,7 +1,12 @@
 const Typography = () => {
   return (
     <>
-      <p>salut</p>
+      <div className="flex flex-col items-baseline p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg mt-12 w-full mb-10">
+        <p className="font-sans">salut</p>
+        <p className="font-serif">salut</p>
+        <p className="font-extrabold">salut</p>
+        <p className="font-mono">salut</p>
+      </div>
     </>
   );
 };
