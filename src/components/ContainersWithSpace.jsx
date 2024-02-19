24cc8c061c11c12818096687a6d346b7c76962a5
Mirko Bozzetto <mirkouch@gmail.com>
2024-02-19 16:20:05 +0100

    Ajouter les dépendances @headlessui/react et client-only, et mettre à jour ContainersWithSpace.jsx

--- src/components/ContainersWithSpace.jsx
+++ src/components/ContainersWithSpace.jsx
@@ -1,8 +1,26 @@
 const ContainersWithSpace = () => {
   return (
     <>
-      <div className="container mx-auto bg-slate-100 mt-10">
-        <article>
+      <div className="container mx-auto bg-slate-100 mt-10 flex flex-row ">
+        <article className="m-4 bg-white p-4">
+          <h3>Article one</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
+            consequatur rem architecto praesentium laboriosam nobis suscipit.
+            Obcaecati mollitia facilis ea neque iure culpa laboriosam aliquam
+            delectus, quia corporis eveniet odio.
+          </p>
+        </article>
+        <article className="m-4 bg-white p-4">
+          <h3>Article one</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
+            consequatur rem architecto praesentium laboriosam nobis suscipit.
+            Obcaecati mollitia facilis ea neque iure culpa laboriosam aliquam
+            delectus, quia corporis eveniet odio.
+          </p>
+        </article>
+        <article className="m-4 bg-white p-4">
           <h3>Article one</h3>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
