8ea93764569f854cd726687a5cd07d5d6e3cb292
Mirko Bozzetto <mirkouch@gmail.com>
2024-02-20 14:22:01 +0100

    Mise à jour des styles de typographie et des décorations

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -15,7 +15,26 @@ const Typography = () => {
         <p className="font-sans text-xs mx-auto">Tailwind</p>
         <p className="font-serif text-xl mx-auto">Tailwind</p>
         <p className="font-extrabold text-lg mx-auto">Tailwind</p>
-        <p className="font-mono text-sm mx-auto">Tailwind</p>
+        <p className="font-mono font-light text-sm mx-auto">Tailwind</p>
+      </div>
+      {/*  */}
+      <div className="flex justify-between items-center">
+        <p className=" text-left px-14 text-red-600">la vie est belle</p>
+        <p className=" text-center px-14 text-blue-600">la vie est belle</p>
+        <p className=" text-right px-14 text-green-600">la vie est belle</p>
+      </div>
+      {/* decorations */}
+
+      <div className="flex justify-between items-center">
+        <p className=" text-left px-14 text-red-600 underline">
+          la vie est belle
+        </p>
+        <p className=" text-center px-14 text-blue-600 line-through">
+          la vie est belle
+        </p>
+        <p className=" text-right px-14 text-green-600 underline decoration-double">
+          la vie est belle
+        </p>
       </div>
     </>
   );
